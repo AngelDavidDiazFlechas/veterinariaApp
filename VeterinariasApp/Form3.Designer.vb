748988119652c47256ea8661c3db71cc4af9c6e5
Stiven Arboleda <stivenarboleda510@gmail.com>
2022-12-11 12:15:46 -0500

    Se agrega el modulo producto en el rol admin y se implementa la opcion de crear un producto

---
+++ VeterinariasApp/Form3.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class NVProducto
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(NVProducto))
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TBValor = New System.Windows.Forms.TextBox()
+        Me.TBDescripcion = New System.Windows.Forms.TextBox()
+        Me.TBStock = New System.Windows.Forms.TextBox()
+        Me.TBProducto = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.BTAgregarP = New System.Windows.Forms.Button()
+        Me.BTCancelar = New System.Windows.Forms.Button()
+        Me.BTLimpiar = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(154, 239)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 15)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "Valor"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(150, 199)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(69, 15)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "Descripcion"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(151, 157)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 15)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Stock"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(151, 119)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 15)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Nombre Producto"
+        '
+        'TBValor
+        '
+        Me.TBValor.Location = New System.Drawing.Point(324, 239)
+        Me.TBValor.Name = "TBValor"
+        Me.TBValor.Size = New System.Drawing.Size(100, 23)
+        Me.TBValor.TabIndex = 15
+        '
+        'TBDescripcion
+        '
+        Me.TBDescripcion.Location = New System.Drawing.Point(324, 199)
+        Me.TBDescripcion.Name = "TBDescripcion"
+        Me.TBDescripcion.Size = New System.Drawing.Size(100, 23)
+        Me.TBDescripcion.TabIndex = 14
+        '
+        'TBStock
+        '
+        Me.TBStock.Location = New System.Drawing.Point(324, 157)
+        Me.TBStock.Name = "TBStock"
+        Me.TBStock.Size = New System.Drawing.Size(100, 23)
+        Me.TBStock.TabIndex = 13
+        '
+        'TBProducto
+        '
+        Me.TBProducto.Location = New System.Drawing.Point(324, 119)
+        Me.TBProducto.Name = "TBProducto"
+        Me.TBProducto.Size = New System.Drawing.Size(100, 23)
+        Me.TBProducto.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(399, 34)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(56, 15)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "Producto"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(481, 107)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(185, 166)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 21
+        Me.PictureBox1.TabStop = False
+        '
+        'BTAgregarP
+        '
+        Me.BTAgregarP.Location = New System.Drawing.Point(150, 357)
+        Me.BTAgregarP.Name = "BTAgregarP"
+        Me.BTAgregarP.Size = New System.Drawing.Size(75, 23)
+        Me.BTAgregarP.TabIndex = 22
+        Me.BTAgregarP.Text = "Agregar"
+        Me.BTAgregarP.UseVisualStyleBackColor = True
+        '
+        'BTCancelar
+        '
+        Me.BTCancelar.Location = New System.Drawing.Point(266, 357)
+        Me.BTCancelar.Name = "BTCancelar"
+        Me.BTCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.BTCancelar.TabIndex = 23
+        Me.BTCancelar.Text = "Cancelar"
+        Me.BTCancelar.UseVisualStyleBackColor = True
+        '
+        'BTLimpiar
+        '
+        Me.BTLimpiar.Location = New System.Drawing.Point(379, 357)
+        Me.BTLimpiar.Name = "BTLimpiar"
+        Me.BTLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.BTLimpiar.TabIndex = 24
+        Me.BTLimpiar.Text = "Limpiar"
+        Me.BTLimpiar.UseVisualStyleBackColor = True
+        '
+        'NVProducto
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 401)
+        Me.Controls.Add(Me.BTLimpiar)
+        Me.Controls.Add(Me.BTCancelar)
+        Me.Controls.Add(Me.BTAgregarP)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TBValor)
+        Me.Controls.Add(Me.TBDescripcion)
+        Me.Controls.Add(Me.TBStock)
+        Me.Controls.Add(Me.TBProducto)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Name = "NVProducto"
+        Me.Text = "Form3"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TBValor As TextBox
+    Friend WithEvents TBDescripcion As TextBox
+    Friend WithEvents TBStock As TextBox
+    Friend WithEvents TBProducto As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents BTAgregarP As Button
+    Friend WithEvents BTCancelar As Button
+    Friend WithEvents BTLimpiar As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents CBMascota As ComboBox
+End Class
--- VeterinariasApp/veterinario.Designer.vb
+++ VeterinariasApp/veterinario.Designer.vb
@@ -24,29 +24,29 @@ Partial Class veterinario
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(veterinario))
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox4 = New System.Windows.Forms.PictureBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
         Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
@@ -65,25 +65,111 @@ Partial Class veterinario
         Me.Panel1.Controls.Add(Me.Label3)
         Me.Panel1.Controls.Add(Me.PictureBox3)
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(291, 567)
+        Me.Panel1.Size = New System.Drawing.Size(255, 425)
         Me.Panel1.TabIndex = 5
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.VeterinariasApp.My.Resources.Resources.searchtext
+        Me.PictureBox8.Location = New System.Drawing.Point(43, 310)
+        Me.PictureBox8.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(30, 26)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 13
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = Global.VeterinariasApp.My.Resources.Resources.catsymboladd
+        Me.PictureBox7.Location = New System.Drawing.Point(43, 265)
+        Me.PictureBox7.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(30, 26)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 12
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.VeterinariasApp.My.Resources.Resources.teamsales
+        Me.PictureBox6.Location = New System.Drawing.Point(43, 216)
+        Me.PictureBox6.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(30, 26)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 11
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.VeterinariasApp.My.Resources.Resources.femaleapprove
+        Me.PictureBox5.Location = New System.Drawing.Point(43, 169)
+        Me.PictureBox5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(30, 26)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 10
+        Me.PictureBox5.TabStop = False
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(78, 311)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(124, 22)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Consultar Macota"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(83, 266)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(106, 22)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Nueva Mascota"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(78, 220)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(121, 22)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Cliente Existente"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(84, 170)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 22)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Nuevo Cliente"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(219, 2)
+        Me.Label1.Location = New System.Drawing.Point(192, 2)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(38, 20)
+        Me.Label1.Size = New System.Drawing.Size(29, 15)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Salir"
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.VeterinariasApp.My.Resources.Resources.cerrars
-        Me.PictureBox1.Location = New System.Drawing.Point(263, 0)
+        Me.PictureBox1.Location = New System.Drawing.Point(230, 0)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(28, 27)
+        Me.PictureBox1.Size = New System.Drawing.Size(24, 20)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
@@ -91,9 +177,9 @@ Partial Class veterinario
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(95, 35)
+        Me.Label3.Location = New System.Drawing.Point(83, 26)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(81, 20)
+        Me.Label3.Size = New System.Drawing.Size(63, 15)
         Me.Label3.TabIndex = 3
         Me.Label3.Text = "Veterinario"
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -101,9 +187,10 @@ Partial Class veterinario
         'PictureBox3
         '
         Me.PictureBox3.Image = Global.VeterinariasApp.My.Resources.Resources.veterinario
-        Me.PictureBox3.Location = New System.Drawing.Point(78, 58)
+        Me.PictureBox3.Location = New System.Drawing.Point(68, 44)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(121, 117)
+        Me.PictureBox3.Size = New System.Drawing.Size(106, 88)
         Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox3.TabIndex = 2
         Me.PictureBox3.TabStop = False
@@ -111,118 +198,45 @@ Partial Class veterinario
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(522, 35)
+        Me.PictureBox2.Location = New System.Drawing.Point(457, 26)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(424, 371)
+        Me.PictureBox2.Size = New System.Drawing.Size(371, 278)
         Me.PictureBox2.TabIndex = 6
         Me.PictureBox2.TabStop = False
         '
         'PictureBox4
         '
         Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
-        Me.PictureBox4.Location = New System.Drawing.Point(608, 433)
+        Me.PictureBox4.Location = New System.Drawing.Point(532, 325)
+        Me.PictureBox4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(254, 94)
+        Me.PictureBox4.Size = New System.Drawing.Size(222, 70)
         Me.PictureBox4.TabIndex = 7
         Me.PictureBox4.TabStop = False
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(96, 226)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(121, 29)
-        Me.Button1.TabIndex = 5
-        Me.Button1.Text = "Nuevo Cliente"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(89, 293)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(138, 29)
-        Me.Button2.TabIndex = 6
-        Me.Button2.Text = "Cliente Existente"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button3
-        '
-        Me.Button3.Location = New System.Drawing.Point(95, 354)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(121, 29)
-        Me.Button3.TabIndex = 7
-        Me.Button3.Text = "Nueva Mascota"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
-        'Button5
-        '
-        Me.Button5.Location = New System.Drawing.Point(89, 415)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(142, 29)
-        Me.Button5.TabIndex = 9
-        Me.Button5.Text = "Consultar Macota"
-        Me.Button5.UseVisualStyleBackColor = True
-        '
-        'PictureBox5
-        '
-        Me.PictureBox5.Image = Global.VeterinariasApp.My.Resources.Resources.femaleapprove
-        Me.PictureBox5.Location = New System.Drawing.Point(49, 225)
-        Me.PictureBox5.Name = "PictureBox5"
-        Me.PictureBox5.Size = New System.Drawing.Size(34, 34)
-        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox5.TabIndex = 10
-        Me.PictureBox5.TabStop = False
-        '
-        'PictureBox6
-        '
-        Me.PictureBox6.Image = Global.VeterinariasApp.My.Resources.Resources.teamsales
-        Me.PictureBox6.Location = New System.Drawing.Point(49, 288)
-        Me.PictureBox6.Name = "PictureBox6"
-        Me.PictureBox6.Size = New System.Drawing.Size(34, 34)
-        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox6.TabIndex = 11
-        Me.PictureBox6.TabStop = False
-        '
-        'PictureBox7
-        '
-        Me.PictureBox7.Image = Global.VeterinariasApp.My.Resources.Resources.catsymboladd
-        Me.PictureBox7.Location = New System.Drawing.Point(49, 353)
-        Me.PictureBox7.Name = "PictureBox7"
-        Me.PictureBox7.Size = New System.Drawing.Size(34, 34)
-        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox7.TabIndex = 12
-        Me.PictureBox7.TabStop = False
-        '
-        'PictureBox8
-        '
-        Me.PictureBox8.Image = Global.VeterinariasApp.My.Resources.Resources.searchtext
-        Me.PictureBox8.Location = New System.Drawing.Point(49, 413)
-        Me.PictureBox8.Name = "PictureBox8"
-        Me.PictureBox8.Size = New System.Drawing.Size(34, 34)
-        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox8.TabIndex = 13
-        Me.PictureBox8.TabStop = False
-        '
         'veterinario
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.ClientSize = New System.Drawing.Size(1153, 563)
+        Me.ClientSize = New System.Drawing.Size(1009, 422)
         Me.Controls.Add(Me.PictureBox4)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "veterinario"
         Me.Text = "veterinario"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
